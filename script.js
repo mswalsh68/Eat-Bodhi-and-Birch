e5b2232f8f1ffb16a3c3797484bd3187679b0f86
mswalsh68 <mswalsh68@gmail.com>
2023-07-18 09:27:58 -0400

    side nav

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-$(".sidebar ul li").on('click', function(){
-    $(".sidebar ul li.active").removeClass('active');
-    $(this).addClass('active');
-});
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
 
-$('.open-btn').on('click', function(){
-    $('.sidebar').addClass('active');
-})
-
-$('.close-btn').on('click', function(){
-    $('.sidebar').removeClass('active');
-})
-
+navToggle.addEventListener('click', ()=> {
+    const visibility = primaryNav.getAttribute('data-visible');
+        console.log(visibility);
+    
+        if (visibility === "false"){
+            primaryNav.setAttribute('data-visible', true)
+            navToggle.setAttribute('aria-expanded', true)
+        }else if (visibility === "true") {
+        primaryNav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', false)
+        }
+    })
