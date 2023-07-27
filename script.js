ffa90bd489e82b871b3381df69907276b08578dd
mswalsh68 <mswalsh68@gmail.com>
2023-07-27 08:23:17 -0400

    new updates

--- script.js
+++ script.js
@@ -12,4 +12,26 @@ navToggle.addEventListener('click', ()=> {
         primaryNav.setAttribute('data-visible', false)
         navToggle.setAttribute('aria-expanded', false)
         }
-    })
+    });
+
+const swiper = new Swiper('.swiper', {
+    autoplay:{
+        delay: 3000, 
+        disableOnInteraction: false,
+    },
+
+    loop: true,
+    
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+    });
